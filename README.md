83e43b90cd2f0e3885a8f1bfe69dd3f93faacba4
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-10-08 17:14:02 +0700

    code base

---
+++ README.md
@@ -0,0 +1,9 @@
+### Hướng dẫn clone Repo code này về máy các bạn chuẩn nhất ở đây nhé:
+
+### Requirements (Semantic Versioning)
+
+```
+* nodejs = v20.12.2
+* npm = v10.5.0
+* yarn = v1.22.19
+```
---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,12 @@
+import { auth } from 'express-oauth2-jwt-bearer';
+
+// Với việc dùng thuật toán RS256, kết hợp express-oauth2-jwt-bearer sẽ tự động xử lý việc lấy và sử dụng khóa công khai từ Auth0 thông qua cơ chế jwks.json, do đó không cần phải cung cấp secret trong quá trình verify JWT.
+const auth0JwtCheck = auth({
+  audience: '',
+  issuerBaseURL: '',
+  tokenSigningAlg: 'RS256',
+});
+
+export const authMiddleware = {
+  auth0JwtCheck,
+};
