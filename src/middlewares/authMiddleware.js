20c55394c1a2a47ad945c3f42a4869d97d611091
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-10-21 22:00:23 +0700

    verify auth0 access token

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,8 +2,8 @@ import { auth } from 'express-oauth2-jwt-bearer';
 
 // Với việc dùng thuật toán RS256, kết hợp express-oauth2-jwt-bearer sẽ tự động xử lý việc lấy và sử dụng khóa công khai từ Auth0 thông qua cơ chế jwks.json, do đó không cần phải cung cấp secret trong quá trình verify JWT.
 const auth0JwtCheck = auth({
-  audience: '',
-  issuerBaseURL: '',
+  audience: 'https://node-server-sso-g7b5.onrender.com',
+  issuerBaseURL: 'https://dev-36l1kpwvov80hkg5.us.auth0.com/',
   tokenSigningAlg: 'RS256',
 });
 
--- src/routes/v1/userRoute.js
+++ src/routes/v1/userRoute.js
@@ -1,23 +1,25 @@
-import express from 'express'
-import { userController } from '~/controllers/userController'
-// import { authMiddleware } from '~/middlewares/authMiddleware'
+import express from 'express';
+import { userController } from '~/controllers/userController';
+import { authMiddleware } from '~/middlewares/authMiddleware';
 
-const Router = express.Router()
+const Router = express.Router();
 
 // Private: API nhận thông tin user từ Auth0 và tạo mới bản ghi vào Database của dự án (cần xác thực accessToken)
-Router.route('/private/hook/login')
-  .post(userController.hookLogin)
+Router.route('/private/hook/login').post(
+  authMiddleware.auth0JwtCheck,
+  userController.hookLogin
+);
 
 // Private: API get toàn bộ user (cần xác thực accessToken)
-Router.route('/private/get_all')
-  .get(userController.getAll)
+Router.route('/private/get_all').get(
+  authMiddleware.auth0JwtCheck,
+  userController.getAll
+);
 
 // Public: API get toàn bộ user (Không cần xác thực accessToken)
-Router.route('/public/get_all')
-  .get(userController.getAll)
+Router.route('/public/get_all').get(userController.getAll);
 
 // Public: API xóa một user theo email (dùng để test xóa user cho nhanh)
-Router.route('/public/delete/:email')
-  .delete(userController.deleteByEmail)
+Router.route('/public/delete/:email').delete(userController.deleteByEmail);
 
-export const userRoute = Router
+export const userRoute = Router;
